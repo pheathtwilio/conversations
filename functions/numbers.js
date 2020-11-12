89c53462315b1e4c9c91b82bccf24f4bfe7892f0
pheath <pheath@twilio.com>
2020-11-12 16:55:26 -0600

    Adding getNumber function

---
+++ functions/numbers.js
@@ -0,0 +1,20 @@
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+
+
+const getNumber = new Promise((resolve, reject) => {
+    client.availablePhoneNumbers('US')
+        .local
+        .list({limit: 1})
+        .then((local) => {
+            resolve(local)
+        })
+        .catch((e) => {
+            reject(e)
+        })
+})
+
+getNumber.then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
